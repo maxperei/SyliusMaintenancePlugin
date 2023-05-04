5887ee2f5be8288ab586c1943d8d8fbfb9a2ecb5
maxperei <maxime.pereira@synolia.com>
2023-05-04 18:10:53 +0200

    add chakra ui and framer motion w/ wavy animation ^^

--- assets/controllers/render_controller.js
+++ assets/controllers/render_controller.js
@@ -1,5 +1,7 @@
 import {default as ReactRender} from 'ux-react/assets/dist/render_controller.js';
-import {createElement} from "react";
+import {createElement, useState} from "react";
+import {ChakraProvider, Box, Text} from '@chakra-ui/react'
+import {motion} from "framer-motion";
 
 export default class extends ReactRender {
     static values = {
@@ -9,9 +11,106 @@ export default class extends ReactRender {
     connect() {
         const props = JSON.parse(this.propsValue);
         super._renderReactElement(createElement(
-            'div',
-            props,
-            `Hello, ${props.fullName}`
+            ChakraProvider,
+            null,
+            createElement(
+                greetings,
+                props,
+            )
         ));
     }
+}
+
+function greetings({fullName}) {
+    const [replay, setReplay] = useState(true);
+
+    setTimeout(() => {
+        setReplay(true)
+    }, 600)
+
+    return createElement(
+        'div',
+        null,
+        createElement(
+            Box,
+            {
+                bgGradient: 'radial(gray.300, yellow.400, pink.200)',
+                w: '100%',
+                h: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            },
+            createElement(wavyText, {
+                text: fullName,
+                delay: 0,
+                duration: 0.05,
+                replay: replay,
+            }),
+        )
+    )
+}
+
+function wavyText({text, delay, duration, replay, ...props}) {
+    const letters = Array.from(text);
+
+    const container = {
+        hidden: {
+            opacity: 0
+        },
+        visible: (i = 1) => ({
+            opacity: 1,
+            transition: {staggerChildren: duration, delayChildren: i * delay}
+        }),
+    }
+
+    const child = {
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                type: "spring",
+                damping: 12,
+                stiffness: 200
+            }
+        },
+        hidden: {
+            opacity: 0,
+            y: 20,
+            transition: {
+                type: "spring",
+                damping: 12,
+                stiffness: 200
+            }
+        }
+    }
+
+    return createElement(
+        motion.h1,
+        {
+            variants: container,
+            style: {display: "flex", overflow: "hidden"},
+            initial: 'hidden',
+            animate: 'visible',
+            ...props
+        },
+        letters.map((letter, index) => {
+            return createElement(motion.span,
+                {key: index, variants: child},
+                letter === " " ? createElement(Text, {
+                    fontFamily: 'system-ui',
+                    bgGradient: 'linear(to-l, #7928CA, #FF0080)',
+                    bgClip: 'text',
+                    fontSize: '6xl',
+                    fontWeight: 'extrabold',
+                }, "\u00A0") : createElement(Text, {
+                    fontFamily: 'system-ui',
+                    bgGradient: 'linear(to-l, #7928CA, #FF0080)',
+                    bgClip: 'text',
+                    fontSize: '6xl',
+                    fontWeight: 'extrabold',
+                }, letter)
+            )
+        })
+    )
 }
